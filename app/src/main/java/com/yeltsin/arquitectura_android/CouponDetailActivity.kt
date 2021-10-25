af458385e9a5d7a02035cbf3b3b10bf3a5da95d4
YeltsinBL <gbl_leo_30@hotmail.com>
2021-10-24 21:20:33 -0500

    Inicio del Proyecto sin Arquitectura

---
+++ app/src/main/java/com/yeltsin/arquitectura_android/CouponDetailActivity.kt
@@ -0,0 +1,14 @@
+package com.yeltsin.arquitectura_android
+
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CouponDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_coupon_detail)
+
+    }
+}
