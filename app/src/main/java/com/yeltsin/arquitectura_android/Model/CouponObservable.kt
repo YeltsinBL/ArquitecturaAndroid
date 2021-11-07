6daa6ab250a11f878cdca89f49a92ec705850900
YeltsinBL <gbl_leo_30@hotmail.com>
2021-11-07 18:14:01 -0500

    Creación de los archivos que servirán de conexión

---
+++ app/src/main/java/com/yeltsin/arquitectura_android/Model/CouponObservable.kt
@@ -0,0 +1,15 @@
+package com.yeltsin.arquitectura_android.Model
+
+import androidx.databinding.BaseObservable
+
+//Conexión directa con el Repository y ViewModel
+class CouponObservable: BaseObservable() {
+
+    //Repository
+
+
+
+
+    //ViewModel
+
+}
---
+++ app/src/main/java/com/yeltsin/arquitectura_android/viewmodel/CouponViewModel.kt
@@ -0,0 +1,11 @@
+package com.yeltsin.arquitectura_android.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+//Maneja los estados de las vistas y
+//Las conexiones del Observable
+class CouponViewModel:ViewModel() {
+
+
+
+}
